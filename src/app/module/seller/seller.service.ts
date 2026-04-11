9e963135f6c61aed201332981d62c8f2f087e198
nirobmondal <nirobmondal202@gmail.com>
2026-04-11 18:51:25 +0600

    adding transaction in creating seller profile

--- src/app/module/seller/seller.service.ts
+++ src/app/module/seller/seller.service.ts
@@ -1,5 +1,5 @@
 import status from "http-status";
-import { Seller } from "../../../generated/prisma/client";
+import { Role, Seller } from "../../../generated/prisma/client";
 import AppError from "../../errorHelpers/AppError";
 
 import { prisma } from "../../lib/prisma";
@@ -15,11 +15,27 @@ const createSellerProfile = async (payload: Seller): Promise<Seller> => {
     throw new AppError(status.CONFLICT, "Seller profile already exists");
   }
 
-  const sellerProfile = await prisma.seller.create({
-    data: {
-      ...payload,
-    },
+  const sellerProfile = await prisma.$transaction(async (tx) => {
+    const seller = await tx.seller.create({
+      data: {
+        ...payload,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    await tx.user.update({
+      where: {
+        id: payload.userId,
+      },
+      data: {
+        role: Role.SELLER,
+      },
+    });
+    return seller;
   });
+
   return sellerProfile;
 };
 
